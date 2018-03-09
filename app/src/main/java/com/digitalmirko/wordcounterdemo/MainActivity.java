1b31c7ad39105067052145e49d1dbeb10e73cc41
Mirko Cukich <mirkocukich@gmail.com>
2018-03-08 22:34:24 -0600

    Initial commit

---
+++ app/src/main/java/com/digitalmirko/wordcounterdemo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.digitalmirko.wordcounterdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String text = editText.getText().toString();
+                text = text.replace("\n", " ");
+                String[] textArray = text.split(" ");
+                textView.setText("Words typed: " + textArray.length);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
